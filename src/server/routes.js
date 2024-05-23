f2d94f276242e516a36c7fb36a29bada222866d1
Ridwan Aji Nugraha <ridwanajinugraha@gmail.com>
2024-05-23 10:20:10 +0700

    first commit

---
+++ src/server/routes.js
@@ -0,0 +1,23 @@
+const {postPredictHandler, getPredictHistoriesHandler, } = require("../server/handler");
+
+const routes = [
+  {
+      path: "/predict",
+      method: "POST",
+      handler: postPredictHandler,
+      options: {
+          payload: {
+              allow: "multipart/form-data",
+              maxBytes: 1000000,
+              multipart: true,
+          },
+      },
+  },
+  {
+      path: "/predict/histories",
+      method: "GET",
+      handler: getPredictHistoriesHandler,
+  },
+];
+
+module.exports = routes;
